fb2cd27948256b9e992f136ffaa6d7ecdc8c898b
Navoda Weerasuriya <navodaweerasuriya1999@gmail.com>
2025-09-27 15:08:09 +0530

    Redirect / to /services

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '**' }
-    ]
-  }
-}
-export default nextConfig
+  async redirects() {
+    return [{ source: '/', destination: '/services', permanent: false }];
+  },
+};
+module.exports = nextConfig;
